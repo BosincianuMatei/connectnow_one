2d366e712ce74af58d92254c60340d1199e5a3d8
MateiBosincianu <bosincianumatei@gmail.com>
2022-07-07 13:03:33 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/connectnow/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/connectnow_one/" : "/",
 })
